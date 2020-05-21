9ab50382210528a403a6dfd4e107db2dda0967fa
= <=>
2020-05-21 03:20:46 -0400

    Started work on the inventory and crafting system

---
+++ Unity Project - Fantasy Game/Assets/Testing/Inventory/CoreItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Weapon Parts/Core")]
+public class CoreItem : Item
+{
+    public CoreItem()
+    { type = ItemType.core; }
+    public CrystalItem crystal;
+
+    public PartMaterial harness;
+    [HideInInspector]
+    public static float harnessEffiency = 1;
+
+    public WeaponPart[] foci;
+
+    public GameObject prefab;
+}
---
+++ Unity Project - Fantasy Game/Assets/Testing/Inventory/WeaponPart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Weapon Parts/Basic Part")]
+public class WeaponPart : Item
+{
+    [Tooltip("0.1, 0.2, 0.25, 0.5, 0.75, 1")]
+    public float materialEffiency = 1;
+    public PartMaterial partMaterial;
+}
